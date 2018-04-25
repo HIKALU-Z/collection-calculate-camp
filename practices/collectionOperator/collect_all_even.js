d00445c32e7d23f37b8785ae99cdb37b995e0b91
HIKALU <play3a@126.com>
2018-04-25 22:43:17 +0800

    finished first task

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,7 +1,27 @@
 'use strict';
 
+/**
+ * 判断参数是否为数组
+ * @param arr
+ * @returns {boolean}
+ */
+function isArray(arr) {
+  return Object.prototype.toString.call(arr) == '[object Array]';
+}
+
+
 function collect_all_even(collection) {
   //在这里写入代码
+  if (!isArray(collection)) {
+    return;
+  }
+  let result = [];
+  collection.forEach(item => {
+    if (item % 2 == 0) {
+    result.push(item);
+  }
+});
+  return result;
 }
 
 module.exports = collect_all_even;
