9eb4cf97ed2c09379dee7ae2ac932598d612a4db
HIKALU <play3a@126.com>
2018-05-04 11:10:54 +0800

    success get_integer_interval

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -1,7 +1,21 @@
-'use strict';
+"use strict";
+
+/**
+ * 判断参数是否为数组
+ * @param arr
+ * @returns {boolean}
+ */
+function isArray(arr) {
+  return Object.prototype.toString.call(arr) == "[object Array]";
+}
 
 function collect_last_element(collection) {
+  if (!isArray(collection)) {
+    return;
+  }
   //在这里写入代码
+  let result = collection[collection.length - 1];
+  return result;
 }
 
 module.exports = collect_last_element;
--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,5 +1,14 @@
 'use strict';
 
+/**
+ * 判断参数是否为数组
+ * @param arr
+ * @returns {boolean}
+ */
+function isArray(arr) {
+  return Object.prototype.toString.call(arr) == "[object Array]";
+}
+
 function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
 }
