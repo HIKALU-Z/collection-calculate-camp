2b7b2ace71ed34fa3d01126ee408fd61755909ec
HIKALU <play3a@126.com>
2018-05-04 18:00:18 +0800

    pass filter_spec

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -3,6 +3,13 @@
 function choose_even(collection) {
 
   //在这里写入代码
+  let result = [];
+  collection.forEach(element => {
+    if(element%2==0){
+      result.push(element)
+    }
+  });
+  return result;
 }
 
 module.exports = choose_even;
--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -3,6 +3,21 @@
 function choose_no_repeat_number(collection) {
 
   //在这里写入代码
+  let result = []
+  for (let i = 0; i < collection.length; i++) {
+    let flag = true;
+    let item = collection[i]
+    for (let j = 0; j < result.length; j++) {
+      if (item === result[j]) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      result.push(item)
+    }
+  }
+  return result;
 }
 
 module.exports = choose_no_repeat_number;
