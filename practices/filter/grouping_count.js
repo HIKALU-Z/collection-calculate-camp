0d77d1914e606dee1aa4782c17a39e152a6b784c
zhanggl <play3a@126.com>
2018-05-07 18:03:32 +0800

    pass new test

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,15 @@
 'use strict';
 
-function grouping_count(collection) {
+function grouping_count(arr) {
 
   //在这里写入代码
+	let result = {};
+	arr.forEach(function (val) {
+		if (result[val] == undefined) {
+			result[val] = 1;
+		} else result[val]++;
+  })
+  return result;
 }
 
 module.exports = grouping_count;
